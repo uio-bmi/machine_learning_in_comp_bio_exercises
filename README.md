e5a28cd0f8c164a118e9ae8512817356974672a1
pavlovicmilena <milenapavlovic775@gmail.com>
2022-10-21 17:36:43 +0200

    make exercise 1

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # Machine Learning in Computational Biology Exercises
 
-This repository contains exercises for the machine learning part of the course.
+This repository contains exercises for the machine learning part of the IN-BIOS5000/IN-BIOS9000 course.
 
 ## Exercise 1: Transcription Factor Binding Prediction
 
-In this exercise, we will have three datasets consisting of DNA sequences which are labeled 0 or 1 if the transcription factor binds to them or 
-not, respectively. We will run a few Python scripts to train the models and then examine the results to try to understand how the models work.
+In this exercise, we will have a dataset consisting of DNA sequences which are labeled 0 or 1 if the transcription factor binds to them or 
+not, respectively. 
 
-Datasets for this exercise were downloaded from https://github.com/QData/DeepMotif.
+We will run Exercise_1.ipnyb notebook to train the models and then examine the results to try to understand how the models work.
+
+The dataset for this exercise was downloaded from https://github.com/QData/DeepMotif.
+
+## Exercise 2: Transcription Factor Binding Prediction -- selecting hyperparameters
+
+In this exercise, we will have the same dataset as in Exercise 1 with the same aim of building a good predictive model. To that aim, 
+we will include cross validation (CV) to explore different hyperparameters and models.
+
+## Exercise 3: predicting disease states from adaptive immune receptor repertoires
+
+Adaptive immune receptors bind to antigens in the body (such as parts of viruses or bacteria) and help neutralize the threat. In the adaptive
+immune receptor repertoire that includes all receptors in the body, there are approximately 10^8 unique receptors that mostly recognize 
+different threats. By using machine learning, it might be possible to predict if a person has a given immune-related disease from their repertoire data.
+
+In this exercise, we will use a public immuneML Galaxy tool to build an ML model that will be able to classify between repertoires coming from
+healthy and diseased individuals.
--- scripts/util.py
+++ scripts/util.py
@@ -1,21 +1,26 @@
 import itertools
+import os
+import random
+import shutil
 from collections import Counter
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.preprocessing import normalize, OneHotEncoder
 
+from plotly.offline import init_notebook_mode, iplot
+import plotly.graph_objects as go
+
 ALPHABET = ["A", "C", "G", "T"]
 
 
-def train_and_assess_logistic_regression(encoded_train, train_labels, encoded_test, test_labels, feature_names, arguments, C):
+def train_and_assess_logistic_regression(encoded_train, train_labels, encoded_test, test_labels, feature_names, result_path, C):
     print("Training logistic regression model started.")
 
-    logistic_regression = LogisticRegression(C=C, penalty='l1', solver="saga", max_iter=1200, n_jobs=4)
+    logistic_regression = LogisticRegression(C=C, penalty='l1', solver="saga", max_iter=50, n_jobs=4)
     logistic_regression.fit(encoded_train, train_labels)
 
     print("Training logistic regression model finished.")
@@ -28,19 +33,30 @@ def train_and_assess_logistic_regression(encoded_train, train_labels, encoded_te
     accuracy = accuracy_score(test_labels, test_predictions)
     print(f"Logistic regression achieved accuracy score: {round(accuracy, 3)} on test set.")
 
-    plot_confusion_matrix(arguments["result_path"], test_labels, test_predictions)
-    plot_top_n_coefficients(logistic_regression, feature_names, arguments['result_path'], 10)
+    plot_confusion_matrix(result_path, test_labels, test_predictions)
+    plot_top_n_coefficients(logistic_regression.coef_.flatten(), 'logistic regression', feature_names, result_path, 10)
+
+    return logistic_regression
 
 
-def print_dataset_info(train_dataset, test_dataset):
+def print_dataset_info(train_dataset, test_dataset=None):
 
-    print(f"Train dataset: {train_dataset.shape[0]} examples, {sum(train_dataset['label'].values)} positive and "
+    print(f"{'Training dataset' if test_dataset is not None else 'Dataset'}: {train_dataset.shape[0]} examples, {sum(train_dataset['label'].values)} positive and "
           f"{train_dataset.shape[0] - sum(train_dataset['label'].values)} negative ones.")
-    print(f"Train dataset: {test_dataset.shape[0]} examples, {sum(test_dataset['label'].values)} positive and "
-          f"{test_dataset.shape[0] - sum(test_dataset['label'].values)} negative ones.")
+
+    print(f"\nPreview of the {'training ' if test_dataset is not None else ''}dataset:")
+    print(train_dataset.head(5).to_string(index=False))
+
+    if test_dataset is not None:
+        print(f"\n\nTest dataset: {test_dataset.shape[0]} examples, {sum(test_dataset['label'].values)} positive and "
+              f"{test_dataset.shape[0] - sum(test_dataset['label'].values)} negative ones.")
 
 
-def load_data(data_path: str) -> pd.DataFrame:
+def load_data_exercise_1():
+    return load_data("data/USF1_K562_USF-1_HudsonAlpha/train.fa", max_examples=500)
+
+
+def load_data(data_path: str, max_examples: int = 100) -> pd.DataFrame:
 
     sequences = []
     labels = []
@@ -55,31 +71,52 @@ def load_data(data_path: str) -> pd.DataFrame:
     assert all(isinstance(label, int) for label in labels)
     assert all(isinstance(seq, str) for seq in sequences)
 
-    return pd.DataFrame({"sequence": sequences, "label": labels})
+    assert np.array_equal(np.unique(labels), [0, 1]), np.unique(labels)
+
+    pos_count = max_examples//2
+    pos_selection = random.sample([i for i in range(len(labels)) if labels[i] == 1], k=pos_count)
+    neg_selection = random.sample([i for i in range(len(labels)) if labels[i] == 0], k=max_examples - pos_count)
+
+    indices_to_keep = pos_selection + neg_selection
+    random.shuffle(indices_to_keep)
+
+    return pd.DataFrame({"sequence": sequences, "label": labels}).iloc[indices_to_keep, :]
 
 
 def encode_kmer(k: int, data: pd.DataFrame, vectorizer, scaler, learn_model: bool, path):
 
     all_kmers = [''.join(x) for x in itertools.product(list(ALPHABET), repeat=k)]
     all_kmers.sort()
-    encoded_kmers = []
-    for row_index, row in data.iterrows():
-        kmers = []
-        for i in range(0, len(row['sequence']) - k + 1, 1):
-            kmers.append(row["sequence"][i:i + k])
-        counter = Counter(kmers) + Counter({kmer: 0 for kmer in all_kmers})
 
-        encoded_kmers.append(dict(counter))
+    # kmer_enc = KmerEncoding(k=k, alphabet_encoding=bionumpy.encodings.DNAEncoding)
+    # kmers = kmer_enc(bionumpy.as_encoded_array(data['sequence'].values.tolist(), target_encoding=bionumpy.encodings.DNAEncoding))
+
+    empty_counter = Counter({kmer: 0 for kmer in all_kmers})
+    encoded_kmers = data.apply(lambda row: dict(Counter([row["sequence"][i:i + k] for i in range(0, len(row['sequence']) - k + 1, 1)])
+                                                + empty_counter),
+                               axis=1).values.tolist()
 
     vectorized_examples = vectorizer.fit_transform(encoded_kmers) if learn_model else vectorizer.transform(encoded_kmers)
     feature_names = vectorizer.get_feature_names()
 
-    normalized_examples = normalize(vectorized_examples, norm='l1')
-    scaled_examples = scaler.fit_transform(normalized_examples) if learn_model else scaler.transform(vectorized_examples)
+    normalized_examples = normalize(vectorized_examples, norm='l2', axis=1)
+    scaled_examples = scaler.fit_transform(normalized_examples) if learn_model else scaler.transform(normalized_examples)
 
-    plot_kmer_frequencies(normalized_examples, data["label"].values, path, learn_model)
+    show_design_matrix(normalized_examples, data['label'].values, path, learn_model, feature_names)
+
+    plot_kmer_frequencies(normalized_examples, data["label"].values, path, learn_model, feature_names)
 
     return scaled_examples, data["label"].values, feature_names
+
+
+def show_design_matrix(examples, labels, path, learn_model, feature_names):
+    df = pd.DataFrame(data=examples, columns=feature_names)
+    df['label'] = labels
+
+    df = df[['label'] + feature_names]
+
+    print(f"{'Train' if learn_model else 'Test'} dataset normalized design matrix preview\n\n")
+    print(df.iloc[:, :7].head(10))
 
 
 def encode_onehot(data: pd.DataFrame):
@@ -100,60 +137,89 @@ def encode_onehot(data: pd.DataFrame):
     return np.array(encoded_data), data["label"].values, feature_names
 
 
-def plot_kmer_frequencies(normalized_examples, labels, path, learn_model):
+def plot_kmer_frequencies(examples, labels, path, learn_model, kmer_names):
 
     unique_labels = np.unique(labels)
     assert unique_labels.shape[0] == 2
 
-    x = normalized_examples[labels == unique_labels[0]]
-    y = normalized_examples[labels == unique_labels[1]]
+    x = np.mean(examples[labels == unique_labels[0]], axis=0)
+    y = np.mean(examples[labels == unique_labels[1]], axis=0)
 
-    fig, ax = plt.subplots()
-    ax.scatter(x, y, c='tab:blue', alpha=0.3, edgecolors='none')
+    max_rng = max(max(x), max(y))
+    max_rng += max_rng*0.01
 
-    ax.grid(True)
+    data = [go.Scatter(x=[0, max_rng], y=[0, max_rng], mode='lines', name='', marker={'color': "lightgrey"}),
+            go.Scatter(x=x, y=y, mode='markers', text=kmer_names, name='', marker={'color': "#636EFA"})]
+    layout = {'title': f'K-mer frequency comparison in {"train" if learn_model else "test"} data',
+              'xaxis_title': f"k-mer frequencies for class {unique_labels[0]}",
+              'yaxis_title': f"k-mer frequencies for class {unique_labels[1]}", 'xaxis_range': [0, max_rng], 'yaxis_range': [0, max_rng],
+              'template': 'plotly_white', 'width': 600, 'height': 600, 'showlegend': False}
 
-    plt.xlabel(f"k-mer frequencies for class {unique_labels[0]}")
-    plt.ylabel(f"k-mer frequencies for class {unique_labels[1]}")
-    plt.savefig(path + f"kmer_freqs_{'train' if learn_model else 'test'}.jpg")
-    plt.clf()
+    fig = go.Figure(data, layout)
+
+    if in_notebook():
+        init_notebook_mode(connected=True)
+        iplot(fig)
+    else:
+        make_fig_obj_from_data(data, layout, path + f"kmer_freqs_{'train' if learn_model else 'test'}.html")
 
 
 def plot_confusion_matrix(path, y_true, predictions):
 
     cm = confusion_matrix(y_true, predictions)
+    layout = {'title': 'Confusion matrix', 'template': 'plotly_white'}
 
-    fig, ax = plt.subplots()
-    ax.imshow(cm)
-    plt.title('TF-binding prediction')
-    plt.ylabel('true class')
-    plt.xlabel('predicted class')
+    data = [go.Heatmap(z=cm[::-1], text=cm[::-1], texttemplate="%{text}", textfont={"size": 20},
+                       customdata=[['false negative', 'true positive'], ['true negative', 'false positive']],
+                       hovertemplate='%{customdata}: %{z} ', name='',
+                       x=[f'predicted class {cls}' for cls in [0, 1]],
+                       y=[f'true class {cls}' for cls in [1, 0]])]
 
-    ax.set_xticks([0, 1])
-    ax.set_yticks([0, 1])
-    ax.set_xticklabels([0, 1])
-    ax.set_yticklabels([0, 1])
-
-    for i in range(2):
-        for j in range(2):
-            ax.text(j, i, cm[i, j], ha='center', va='center', color='w', size=30)
-
-    plt.savefig(f'{path}confusion_matrix.jpg')
-    plt.clf()
+    if in_notebook():
+        init_notebook_mode(connected=True)
+        iplot({'data': data, 'layout': layout})
+    else:
+        make_fig_obj_from_data(data, layout, path + "confusion_matrix.html")
 
 
-def plot_top_n_coefficients(logistic_regression: LogisticRegression, feature_names, path, n):
-
-    coefficients = logistic_regression.coef_.flatten()
+def plot_top_n_coefficients(coefficients: np.ndarray, model_name: str, feature_names, path, n):
 
     top_n_indices = np.argsort(np.abs(coefficients))[-n:]
+    data = [go.Bar(x=np.array(feature_names)[top_n_indices], y=coefficients[top_n_indices])]
+    layout = {'title': f'Top {n} coefficients in {model_name}', 'xaxis_title': 'features', 'yaxis_title': 'coefficient values',
+              'template': 'plotly_white'}
 
-    plt.bar(range(n), coefficients[top_n_indices])
-    plt.xticks(range(n), np.array(feature_names)[top_n_indices])
-    plt.xlabel("features")
-    plt.grid(True)
+    if in_notebook():
+        init_notebook_mode(connected=True)
+        iplot({'data': data, 'layout': layout})
+    else:
+        make_fig_obj_from_data(data, layout, path + f"top_{n}_coefficients.jpg")
 
-    plt.savefig(path + f"top_{n}_coefficients.jpg")
-    plt.clf()
 
+def make_folder(path: str):
+    if os.path.isdir(path):
+        print(f"Removing old folder {path}")
+        shutil.rmtree(path)
+
+    os.makedirs(path)
+    print(f"Made folder {path}")
+
+
+def in_notebook():
+    try:
+        from IPython import get_ipython
+        if 'IPKernelApp' not in get_ipython().config:  # pragma: no cover
+            return False
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+    return True
+
+
+def make_fig_obj_from_data(data, layout, file_path):
+    fig = go.Figure()
+    fig.add_trace(data)
+    fig.update_layout(layout)
+    fig.write_html(file_path)
 
