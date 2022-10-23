0f8c41231a45009ea2e5efff9653343fadc39c64
pavlovicmilena <milenapavlovic775@gmail.com>
2022-10-23 17:35:57 +0200

    add second exercise

--- scripts/util.py
+++ scripts/util.py
@@ -1,15 +1,19 @@
 import itertools
 import os
+import pickle
 import random
 import shutil
 from collections import Counter
+from pathlib import Path
+from typing import Tuple
 
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction import DictVectorizer
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.preprocessing import normalize, OneHotEncoder
+from sklearn.preprocessing import normalize, OneHotEncoder, StandardScaler
 
 from plotly.offline import init_notebook_mode, iplot
 import plotly.graph_objects as go
@@ -17,7 +21,7 @@ import plotly.graph_objects as go
 ALPHABET = ["A", "C", "G", "T"]
 
 
-def train_and_assess_logistic_regression(encoded_train, train_labels, encoded_test, test_labels, feature_names, result_path, C):
+def train_logistic_regression(encoded_train, train_labels, C):
     print("Training logistic regression model started.")
 
     logistic_regression = LogisticRegression(C=C, penalty='l1', solver="saga", max_iter=50, n_jobs=4)
@@ -25,24 +29,26 @@ def train_and_assess_logistic_regression(encoded_train, train_labels, encoded_te
 
     print("Training logistic regression model finished.")
 
-    accuracy = accuracy_score(train_labels, logistic_regression.predict(encoded_train))
+    return logistic_regression
+
+
+def assess_model(model, encoded_train, train_labels, encoded_test, test_labels, feature_names, result_path):
+    accuracy = accuracy_score(train_labels, model.predict(encoded_train))
     print(f"Logistic regression achieved accuracy score: {round(accuracy, 3)} on training set.")
 
-    test_predictions = logistic_regression.predict(encoded_test)
+    test_predictions = model.predict(encoded_test)
 
     accuracy = accuracy_score(test_labels, test_predictions)
     print(f"Logistic regression achieved accuracy score: {round(accuracy, 3)} on test set.")
 
     plot_confusion_matrix(result_path, test_labels, test_predictions)
-    plot_top_n_coefficients(logistic_regression.coef_.flatten(), 'logistic regression', feature_names, result_path, 10)
-
-    return logistic_regression
+    plot_top_n_coefficients(model.coef_.flatten(), model.__class__.__name__, feature_names, result_path, 10)
 
 
 def print_dataset_info(train_dataset, test_dataset=None):
-
-    print(f"{'Training dataset' if test_dataset is not None else 'Dataset'}: {train_dataset.shape[0]} examples, {sum(train_dataset['label'].values)} positive and "
-          f"{train_dataset.shape[0] - sum(train_dataset['label'].values)} negative ones.")
+    print(
+        f"{'Training dataset' if test_dataset is not None else 'Dataset'}: {train_dataset.shape[0]} examples, {sum(train_dataset['label'].values)} positive and "
+        f"{train_dataset.shape[0] - sum(train_dataset['label'].values)} negative ones.")
 
     print(f"\nPreview of the {'training ' if test_dataset is not None else ''}dataset:")
     print(train_dataset.head(5).to_string(index=False))
@@ -56,8 +62,15 @@ def load_data_exercise_1():
     return load_data("data/USF1_K562_USF-1_HudsonAlpha/train.fa", max_examples=500)
 
 
-def load_data(data_path: str, max_examples: int = 100) -> pd.DataFrame:
+def load_data_exercise_2():
+    return load_data("data/USF1_K562_USF-1_HudsonAlpha/train.fa", max_examples=1000)
 
+
+def load_test_dataset_ex2():
+    return load_data("data/USF1_K562_USF-1_HudsonAlpha/test.fa", max_examples=1000)
+
+
+def load_data(data_path: str, max_examples: int = 100) -> pd.DataFrame:
     sequences = []
     labels = []
 
@@ -73,7 +86,7 @@ def load_data(data_path: str, max_examples: int = 100) -> pd.DataFrame:
 
     assert np.array_equal(np.unique(labels), [0, 1]), np.unique(labels)
 
-    pos_count = max_examples//2
+    pos_count = max_examples // 2
     pos_selection = random.sample([i for i in range(len(labels)) if labels[i] == 1], k=pos_count)
     neg_selection = random.sample([i for i in range(len(labels)) if labels[i] == 0], k=max_examples - pos_count)
 
@@ -83,13 +96,40 @@ def load_data(data_path: str, max_examples: int = 100) -> pd.DataFrame:
     return pd.DataFrame({"sequence": sequences, "label": labels}).iloc[indices_to_keep, :]
 
 
-def encode_kmer(k: int, data: pd.DataFrame, vectorizer, scaler, learn_model: bool, path):
+def make_pickle_file_path(path, obj_name, params):
+    return Path(path) / (f"{obj_name}_" + "_".join(str(k) + "_" + str(v) for k, v in params.items()) + ".pickle")
 
+
+def load_vectorizer_scaler(path: str, k: int, learn_model: bool, split: int):
+    vect_path = make_pickle_file_path(path, 'vect', {'k': k, "split": split})
+    scaler_path = make_pickle_file_path(path, 'scaler', {'k': k, "split": split})
+
+    if learn_model and any(p.is_file() for p in [vect_path, scaler_path]):
+        raise RuntimeError(f"Overwriting existing files, check the paths and try again: {vect_path}, {scaler_path}")
+    elif learn_model:
+        return DictVectorizer(sparse=False, dtype=float), StandardScaler()
+    elif not learn_model and all(p.is_file() for p in [vect_path, scaler_path]):
+        out_objs = []
+        for p in [vect_path, scaler_path]:
+            with open(p, 'rb') as file:
+                out_objs.append(pickle.load(file))
+        return out_objs
+    else:
+        raise RuntimeError(f"Missing files {vect_path}, {scaler_path}.")
+
+
+def store_vectorizer_scaler(vectorizer, scaler, path, k, split):
+    with open(make_pickle_file_path(path, 'vect', {'k': k, 'split': split}), 'wb') as file:
+        pickle.dump(vectorizer, file)
+    with open(make_pickle_file_path(path, 'scaler', {'k': k, 'split': split}), 'wb') as file:
+        pickle.dump(scaler, file)
+
+
+def encode_kmer(k: int, data: pd.DataFrame, learn_model: bool, path: str, split: int = 1):
     all_kmers = [''.join(x) for x in itertools.product(list(ALPHABET), repeat=k)]
     all_kmers.sort()
 
-    # kmer_enc = KmerEncoding(k=k, alphabet_encoding=bionumpy.encodings.DNAEncoding)
-    # kmers = kmer_enc(bionumpy.as_encoded_array(data['sequence'].values.tolist(), target_encoding=bionumpy.encodings.DNAEncoding))
+    vectorizer, scaler = load_vectorizer_scaler(path, k, learn_model, split)
 
     empty_counter = Counter({kmer: 0 for kmer in all_kmers})
     encoded_kmers = data.apply(lambda row: dict(Counter([row["sequence"][i:i + k] for i in range(0, len(row['sequence']) - k + 1, 1)])
@@ -105,6 +145,8 @@ def encode_kmer(k: int, data: pd.DataFrame, vectorizer, scaler, learn_model: boo
     show_design_matrix(normalized_examples, data['label'].values, path, learn_model, feature_names)
 
     plot_kmer_frequencies(normalized_examples, data["label"].values, path, learn_model, feature_names)
+
+    store_vectorizer_scaler(vectorizer, scaler, path, k, split)
 
     return scaled_examples, data["label"].values, feature_names
 
@@ -132,13 +174,12 @@ def encode_onehot(data: pd.DataFrame):
     feature_names = []
     for i in range(len(data['sequence'][0])):
         for j in range(len(ALPHABET)):
-            feature_names.append(f"{ALPHABET[j]}_{i+1}")
+            feature_names.append(f"{ALPHABET[j]}_{i + 1}")
 
     return np.array(encoded_data), data["label"].values, feature_names
 
 
 def plot_kmer_frequencies(examples, labels, path, learn_model, kmer_names):
-
     unique_labels = np.unique(labels)
     assert unique_labels.shape[0] == 2
 
@@ -146,7 +187,7 @@ def plot_kmer_frequencies(examples, labels, path, learn_model, kmer_names):
     y = np.mean(examples[labels == unique_labels[1]], axis=0)
 
     max_rng = max(max(x), max(y))
-    max_rng += max_rng*0.01
+    max_rng += max_rng * 0.01
 
     data = [go.Scatter(x=[0, max_rng], y=[0, max_rng], mode='lines', name='', marker={'color': "lightgrey"}),
             go.Scatter(x=x, y=y, mode='markers', text=kmer_names, name='', marker={'color': "#636EFA"})]
@@ -165,7 +206,6 @@ def plot_kmer_frequencies(examples, labels, path, learn_model, kmer_names):
 
 
 def plot_confusion_matrix(path, y_true, predictions):
-
     cm = confusion_matrix(y_true, predictions)
     layout = {'title': 'Confusion matrix', 'template': 'plotly_white'}
 
@@ -183,7 +223,6 @@ def plot_confusion_matrix(path, y_true, predictions):
 
 
 def plot_top_n_coefficients(coefficients: np.ndarray, model_name: str, feature_names, path, n):
-
     top_n_indices = np.argsort(np.abs(coefficients))[-n:]
     data = [go.Bar(x=np.array(feature_names)[top_n_indices], y=coefficients[top_n_indices])]
     layout = {'title': f'Top {n} coefficients in {model_name}', 'xaxis_title': 'features', 'yaxis_title': 'coefficient values',
@@ -222,4 +261,3 @@ def make_fig_obj_from_data(data, layout, file_path):
     fig.add_trace(data)
     fig.update_layout(layout)
     fig.write_html(file_path)
-
